f416d59eaf4eeb5114e4f9f7c0e4cff9ceaffe96
Chernykh-V-N <chernykh.v.n@mail.ru>
2022-10-12 22:27:11 +0700

    weak_ptr

---
+++ weak_ptr/Ravesli_Training/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <memory> // для std::shared_ptr
+
+class Item
+{
+public:
+	std::weak_ptr<Item> m_ptr; // изначально пустой
+
+	Item() { std::cout << "Item acquired\n"; }
+	~Item() { std::cout << "Item destroyed\n"; }
+};
+
+int main()
+{
+	auto ptr1 = std::make_shared<Item>();
+
+	ptr1->m_ptr = ptr1; // m_ptr теперь является владельцем Item-а, членом которого он является сам
+
+	return 0;
+}
